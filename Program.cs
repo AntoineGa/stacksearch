ea124dbb970e6a5e4c27508434e26406a248d2fa
Matías Quaranta <ealsur@hotmail.com>
2015-12-23 00:04:29 -0300

    Added colors and logo

--- Program.cs
+++ Program.cs
@@ -7,17 +7,42 @@ namespace stacksearch
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static int Main(string[] args)
         {
+            var currentColor = Console.ForegroundColor;
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.DarkCyan;
+            Console.WriteLine("                  ++++++                ");
+            Console.WriteLine("                ++++++++++++             ");
+            Console.WriteLine("              ++++++++++++++++           ");
+            Console.WriteLine("              ++++++++++++++++           ");
+            Console.WriteLine("             ++++++       :+++++++++     ");
+            Console.WriteLine("          ++ ++++. +++++++  +++++++++    ");
+            Console.WriteLine("        ++++++++~ ++++++++++ ++++++++    ");
+            Console.WriteLine("       +++++++++ +++++++++++  +++++++    ");
+            Console.WriteLine("    ++++++++++++ ++++++++++++ +++++++    ");
+            Console.WriteLine("  ++++++++++++++ +++++++++++  +++++++++  ");
+            Console.WriteLine(" ++++++++++++++++ ++++++++++ +++++++++++ ");
+            Console.WriteLine(" +++++++++++++++   .++++++  ++++++++++++ ");
+            Console.WriteLine(" +++++++++++++   ++~      +++++++++++++++");
+            Console.WriteLine(" ++++++++++++  ~+++++++++++++++++++++++++");
+            Console.WriteLine(" ++++++++++   ++++++++++++++++++++++++++ ");
+            Console.WriteLine("  +++++++++++++++++++++++++++++++++++++  ");
+            Console.WriteLine("   +++++++++++++++++++++++++++++++++++   ");
+            Console.WriteLine("     +++++++++++++++++++++++++++++++     ");
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.ForegroundColor = ConsoleColor.Cyan;
             Console.WriteLine("StackExchange CommandLine - Azure Search");
             Console.WriteLine("----------------------------------------");
             
-           
+           Console.ForegroundColor = currentColor;
             while(true){
                 Console.Write("Ingrese un texto a buscar y presione ENTER o EXIT para terminar:");
                 var text = Console.ReadLine();
                 if("exit".Equals(text.ToLowerInvariant()))break;
-                Console.WriteLine("Buscando [{0}]...", text);
+                Console.Clear();
+                WriteColorAndReturn(currentColor, ConsoleColor.Cyan, string.Format("Buscando [{0}]...", text),true);
                 var results = SearchService.SearchDocuments(text,string.Empty, string.Empty);
                 if(!results.Any()){
                     Console.WriteLine("No se han encontrado resultados :(");
@@ -25,18 +50,38 @@ namespace stacksearch
                 }
                 else{
                     foreach(var entry in results){
-                        Console.WriteLine("Titulo: {0}", entry.Title);
-                        Console.WriteLine("Respuestas: {0}", entry.AnswerCount);
-                        Console.WriteLine("Comentarios: {0}", entry.CommentCount);
-                        Console.WriteLine("Creada: {0}", entry.CreationDate.ToString("dd/MM/yyyy HH:mm"));
+                        WriteColorAndReturn(currentColor, ConsoleColor.DarkCyan, "Titulo: ");
+                        Console.WriteLine(entry.Title);
+                        WriteColorAndReturn(currentColor, ConsoleColor.DarkCyan, "Respuestas: ");
+                        Console.WriteLine(entry.AnswerCount);
+                        WriteColorAndReturn(currentColor, ConsoleColor.DarkCyan, "Comentarios: ");
+                        Console.WriteLine(entry.CommentCount);
+                        WriteColorAndReturn(currentColor, ConsoleColor.DarkCyan, "Creada: ");
+                        Console.WriteLine(entry.CreationDate.ToString("dd/MM/yyyy HH:mm"));
+                        WriteColorAndReturn(currentColor, ConsoleColor.DarkCyan, "Ultima actividad: ");
                         Console.WriteLine("Ultima actividad: {0}", entry.LastActivityDate.ToString("dd/MM/yyyy HH:mm"));
-                        Console.WriteLine("Pregunta: {0}", entry.Body);
+                        WriteColorAndReturn(currentColor, ConsoleColor.DarkCyan, "Pregunta: ");
+                        Console.WriteLine(entry.Body);
                         Console.WriteLine("----------------------------------");
-                        Console.WriteLine("<Más resultados>");
+                        WriteColorAndReturn(currentColor, ConsoleColor.Cyan, "<Presione una tecla para más resultados>",true);
                         Console.ReadKey();
                     }
                 }
             }
+            return 0;
+        }
+        
+        private static void WriteColorAndReturn(ConsoleColor currentColor, ConsoleColor colorToUse, string text, bool returnLine = false){
+            
+            Console.ForegroundColor = colorToUse;
+            if(returnLine){
+                Console.WriteLine(text);    
+            }else{
+                Console.Write(text);
+            }
+            Console.ForegroundColor = currentColor;
         }
     }
+    
+    
 }
