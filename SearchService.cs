4965af426b48c881f02f61183d5bc5aeef5449fb
Matías Quaranta <ealsur@hotmail.com>
2015-12-23 00:04:20 -0300

    Refactor with no AzureSDK

--- SearchService.cs
+++ SearchService.cs
@@ -1,45 +1,64 @@
 using System.Collections.Generic;
-using Microsoft.Azure.Search;
-using Microsoft.Azure.Search.Models;
 using System.Linq;
 using System;
+using System.Net;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace stacksearch{
     
-    public static class SearchService{
-        static readonly SearchServiceClient client;
-        static readonly SearchIndexClient indexClient;
-        static SearchService()
-        {
-            client = new SearchServiceClient("azs-playground", new SearchCredentials("252044BE3886FE4A8E3BAA4F595114BB"));
-            indexClient = client.Indexes.GetClient("stackexchange");
+    public class SimpleSearchPayload
+    {
+        public SimpleSearchPayload(){
+            Facets = new List<string>();
+            Filter = string.Empty;
+            Count=false;
+            Search=string.Empty;
         }
+        [JsonPropertyAttribute("count")]
+        public bool Count { get; set; }
+        [JsonPropertyAttribute("facets")]
+        public List<string> Facets { get; set; }
         
+        [JsonPropertyAttribute("filter")]
+        public string Filter { get; set; }
+        [JsonPropertyAttribute("search")]
+        public string Search { get; set; }
+    }
+    
+    
+    public class SimpleSearchResponse{
+        
+        [JsonPropertyAttribute("value")]
+        public List<JObject> Value{get;set;}
+    }
+    
+    
+    public static class SearchService{
+        private readonly static string ServiceUrl = "https://azs-playground.search.windows.net/indexes/stackexchange/docs/search?api-version=2015-02-28";
+       
         public static List<Entry> SearchDocuments(string searchText, string filter, string orderBy, int page = 1, int pageSize = 10)
         {
-            
-            var sp = new SearchParameters();
-
-            if (!string.IsNullOrEmpty(filter))
+                
+            using (WebClient wc = new WebClient())
             {
-                sp.Filter = filter;
-            }
-            sp.Top = pageSize;
-            sp.Skip = (page - 1) * pageSize;
-            if (!string.IsNullOrEmpty(orderBy))
-            {
-                sp.OrderBy = orderBy.Split(',');
-            }
-            sp.IncludeTotalResultCount = true;
-            var results = indexClient.Documents.Search(searchText, sp);
-            return results.Select(x=>new Entry(){
-                    AnswerCount = (long)x.Document["answerCount"],
-                    CommentCount = (long)x.Document["commentCount"],
-                    Title = (string)x.Document["title"],
-                    Body = (string)x.Document["body"],
-                    CreationDate = ((DateTimeOffset)x.Document["creationDate"]).DateTime,
-                    LastActivityDate = ((DateTimeOffset)x.Document["lastActivityDate"]).DateTime,
+                wc.Headers[HttpRequestHeader.ContentType] = "application/json";
+                wc.Headers[HttpRequestHeader.Accept] = "application/json";
+                wc.Headers["api-key"] = "252044BE3886FE4A8E3BAA4F595114BB";
+                var payload= new SimpleSearchPayload();
+                payload.Count=true;
+                payload.Search = searchText;
+                string response = wc.UploadString(ServiceUrl, JsonConvert.SerializeObject(payload));
+                var results = JsonConvert.DeserializeObject<SimpleSearchResponse>(response);
+                return results.Value.Select(x=>new Entry(){
+                    AnswerCount = (long)x["answerCount"],
+                    CommentCount = (long)x["commentCount"],
+                    Title = (string)x["title"],
+                    Body = (string)x["body"],
+                    CreationDate = ((DateTimeOffset)x["creationDate"]).DateTime,
+                    LastActivityDate = ((DateTimeOffset)x["lastActivityDate"]).DateTime,
             }).ToList();
+            }
         }
     }
 }
